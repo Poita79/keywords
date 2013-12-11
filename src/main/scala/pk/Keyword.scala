871af434349ead76349acfe2feb650140cca6e53
Pete <mrkneller@hotmail.com>
2013-12-11 16:48:57 +0000

    Primitives should now work correctly between Java/Scala types

--- src/main/scala/pk/Keyword.scala
+++ src/main/scala/pk/Keyword.scala
@@ -6,26 +6,38 @@ trait Keyword[V] extends (Map[String, Any] => V) { self =>
 
   trait TestFailure
 
-  private def noElementError(candidate: Map[String, Any]): RuntimeException = {
-    new NoSuchElementException(s"Keyword $key does not exist in ${candidate.toString}")
-  }
+  def get(candidate: Map[String, Any]): Option[V] = extractValue(candidate).right.toOption
 
-  private def wrongTypeError(candidate: Map[String, Any], value: Any): RuntimeException = {
-    new ClassCastException(s"Keyword '$key' of type ${aClass.getCanonicalName} " +
-      s"cannot be used to access value of type ${value.getClass.getCanonicalName} in ${candidate.toString}")
-  }
+  def apply(candidate: Map[String, Any]): V = extractValue(candidate).fold(ex => throw ex, identity)
 
-  private def test(candidate: Map[String, Any]): Either[RuntimeException,V] = {
-    candidate.get(key).toRight(noElementError(candidate)).right flatMap { v =>
-      if (aClass.isAssignableFrom(v.getClass)) Right(v.asInstanceOf[V])
-      else Left(wrongTypeError(candidate, v))
+
+  private def extractValue(candidate: Map[String, Any]): Either[RuntimeException,V] = {
+
+    def cast(value: Any): Either[RuntimeException, V] = {
+      if (equivalent(aClass, value.getClass))
+        Right(value.asInstanceOf[V])
+      else
+        Left(wrongTypeError(value))
     }
+
+    def equivalent(to: Class[_], from: Class[_]): Boolean = {
+      Keyword.equivalentTypes.contains(from -> to) || to.isAssignableFrom(from)
+    }
+
+    def noElementError: RuntimeException = {
+      new NoSuchElementException(s"Keyword $key does not exist in ${candidate.toString}")
+    }
+
+    def wrongTypeError(value: Any): RuntimeException = {
+      new ClassCastException(s"Keyword '$key' of type ${aClass.getCanonicalName} " +
+        s"cannot be used to access value of type ${value.getClass.getCanonicalName} in ${candidate.toString}")
+    }
+
+    for {
+      rawValue <- candidate.get(key).toRight(noElementError).right
+      castValue <- cast(rawValue).right
+    } yield castValue
   }
-
-  def get(candidate: Map[String, Any]): Option[V] = test(candidate).right.toOption
-
-  def apply(candidate: Map[String, Any]): V = test(candidate).fold(ex => throw ex, identity)
-
 }
 
 object Keyword {
@@ -34,4 +46,15 @@ object Keyword {
     val key = name
     val aClass = implicitly[Manifest[V]].runtimeClass.asInstanceOf[Class[V]]
   }
+
+  private val equivalentTypes = Seq(
+    classOf[java.lang.Byte] -> classOf[scala.Byte],
+    classOf[java.lang.Character] -> classOf[scala.Char],
+    classOf[java.lang.Boolean] -> classOf[scala.Boolean],
+    classOf[java.lang.Short] -> classOf[scala.Short],
+    classOf[java.lang.Integer] -> classOf[scala.Int],
+    classOf[java.lang.Long] -> classOf[scala.Long],
+    classOf[java.lang.Float] -> classOf[scala.Float],
+    classOf[java.lang.Double] -> classOf[scala.Double]
+  )
 }
