4436cb17d8dad6328575fb5932e62af60f072e37
Poita79 <mrkneller@hotmail.com>
2013-12-31 14:26:49 +0000

    remove logging format

--- sbt.sh
+++ sbt.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 JAVA_OPTS="-Xmx512m -XX:MaxPermSize=128m -XX:ReservedCodeCacheSize=128m -cp sbt-launch-0.13.0.jar:jansi-1.11.jar"
+SBT_OPTS="-Dsbt.log.noformat=true"
 
-exec java $JAVA_OPTS xsbt.boot.Boot "$@"
+exec java $JAVA_OPTS $SBT_OPTS xsbt.boot.Boot "$@"
